2205cc5013ab79693e1917950e7f0ac30df30095
massaaki <eu@massaaki.com>
2022-04-23 14:26:54 -0300

    refactor(graphql/server): remove prisma to server.js

--- 01-graphql/server/src/server.ts
+++ 01-graphql/server/src/server.ts
@@ -1,27 +1,11 @@
-import { ApolloServer, gql } from 'apollo-server';
-
-import {MongoDbClient} from './services/db/mongodb-client';
+import { ApolloServer } from 'apollo-server';
 import {typeDefs, resolvers} from './schemas';
-
-
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers
 });
 
-
-
-(async () => {
-	const client = MongoDbClient.getInstance();
-	const allUsers = await client.user.findMany();
-
-	console.log(allUsers);
-
-})()
-
-
-
-// server.listen(4000).then(({ url }) => {
-// 	console.log(`Server is running at ${url}`);
-// });
+server.listen(4000).then(({ url }) => {
+	console.log(`Server is running at ${url}`);
+});
