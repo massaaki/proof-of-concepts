d2eb5911fe8d7d357595726faff0a3f56ef63f6c
massaaki <eu@massaaki.com>
2022-04-23 14:15:17 -0300

    chore(graphql/server): change database to posrgres

--- 01-graphql/server/src/server.ts
+++ 01-graphql/server/src/server.ts
@@ -1,12 +1,27 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+import {MongoDbClient} from './services/db/mongodb-client';
 import {typeDefs, resolvers} from './schemas';
+
+
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers
 });
 
-server.listen(4000).then(({ url }) => {
-	console.log(`Server is running at ${url}`);
-});
+
+
+(async () => {
+	const client = MongoDbClient.getInstance();
+	const allUsers = await client.user.findMany();
+
+	console.log(allUsers);
+
+})()
+
+
+
+// server.listen(4000).then(({ url }) => {
+// 	console.log(`Server is running at ${url}`);
+// });
