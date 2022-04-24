059adbf443de446fe7276f3bc9ec4e8771e0b7ff
massaaki <eu@massaaki.com>
2022-04-24 16:56:24 -0300

    refactor(graphq/server): move repository to context

--- 01-graphql/server/src/schemas/resolvers.ts
+++ 01-graphql/server/src/schemas/resolvers.ts
@@ -1,9 +1,7 @@
-import { User } from "@prisma/client";
+import { User } from '../interfaces/user';
 import { Comment } from "../interfaces/comment";
 import { Post } from "../interfaces/post";
-import { CommentRepository } from "../services/db/repositories/comment-repository";
-import { PostRepository } from "../services/db/repositories/post-repository";
-import { UserRepository } from "../services/db/repositories/user-repository";
+import { Context } from "../server";
 
 type CreatePostInput = {
 	authorId: string;
@@ -11,38 +9,34 @@ type CreatePostInput = {
 
 export const resolvers = {
 	Query: {
-		list_users: async () => {
-			const userRepository = new UserRepository();
+		list_users: async (parent, args, { userRepository }: Context) => {
 			const users = await userRepository.findAll();
 
 			return users;
 		},
-		find_user: async (parent, { id }, context) => {
-			const userRepository = new UserRepository();
+		find_user: async (parent, { id }, { userRepository }: Context) => {
 			const user = await userRepository.find(id);
 
 			if (!user)
 				return null;
 			return user;
 		},
-		list_posts: async (parent, args, context) => {
-			const postRepository = new PostRepository();
+		list_posts: async (parent, args, { postRepository }: Context) => {
 			const post = await postRepository.findAll();
 
 			if (!post)
 				return null;
 			return post;
 		},
-		find_post: async (parent, { id }, context) => {
-			const postRepository = new PostRepository();
+		find_post: async (parent, { id }, { postRepository }: Context) => {
 			const post = await postRepository.find(id);
 
 			if (!post)
 				return null;
+		
 			return post;
 		},
-		list_comments: async (parent, args, context) => {
-			const commentRepository = new CommentRepository();
+		list_comments: async (parent, args, { commentRepository }: Context) => {
 			const comments = await commentRepository.findAll();
 
 			if (!comments)
@@ -50,41 +44,34 @@ export const resolvers = {
 			
 			return comments;
 		},
-		find_comment: async (parent, { id }, context) => {
-			const commentRepository = new CommentRepository();
-
+		find_comment: async (parent, { id }, { commentRepository }: Context) => {
 			const comment = await commentRepository.find(id);
 
 			if (!comment)
 				return null;
 			return comment;
 		}
-
 	},
 	Mutation: {
-		create_user: async (parent, {input}, context) => {
+		create_user: async (parent, { input }, { userRepository }: Context) => {
 			const {name, email} = input as Omit<User, 'id'>;
-			
-			const userRepository = new UserRepository();
+		
 			const user = await userRepository.create({ email, name });
 			return user;
 		},
-		create_post: async (parent, {input}, context) => {
+		create_post: async (parent, { input }, { postRepository }: Context) => {
 			const { authorId, title, body } = input as CreatePostInput;
 
-			const postRepository = new PostRepository();
 			const post = await postRepository.create({
 				authorId, title, body
 			});
 
 			return post;
 		},
-		create_comment: async (parent, { input }, context) => {
+		create_comment: async (parent, { input }, { commentRepository }: Context) => {
 			const { comment, postId } = input as Omit<Comment, 'id'>
-			
-			const commentRepository = new CommentRepository();
-			const response = await commentRepository.create({ comment, postId });
 
+			const response = await commentRepository.create({ comment, postId });
 			return response;
 		}
 	}
--- 01-graphql/server/src/server.ts
+++ 01-graphql/server/src/server.ts
@@ -1,9 +1,24 @@
 import { ApolloServer } from 'apollo-server';
-import {typeDefs, resolvers} from './schemas';
+import { typeDefs, resolvers } from './schemas';
+import { UserRepository } from './services/db/repositories/user-repository';
+import { PostRepository } from './services/db/repositories/post-repository';
+import { CommentRepository } from './services/db/repositories/comment-repository';
+
+
+export type Context = {
+	userRepository: UserRepository;
+	postRepository: PostRepository;
+	commentRepository: CommentRepository;
+}
 
 const server = new ApolloServer({
 	typeDefs,
-	resolvers
+	resolvers,
+	context: {
+		userRepository: new UserRepository(),
+		postRepository: new PostRepository(),
+		commentRepository: new CommentRepository()
+	} as Context
 });
 
 server.listen(4000).then(({ url }) => {
